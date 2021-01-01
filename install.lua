84705dd31d93108001e80a56516704dc29f9aa4a
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-01-01 15:18:32 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,2 +1,3 @@
 shell.run("wget https://raw.githubusercontent.com/RubenHetKonijn/musicify/main/musicify.lua")
 shell.run("wget https://raw.githubusercontent.com/SquidDev-CC/mbs/master/mbs.lua .mbs.lua")
+shell.run(".mbs.lua install")
