b67548ab6bd4b869c1726cf4ac52fb6d70675de5
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-01-01 15:17:34 +0100

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,2 @@
+shell.run("wget https://raw.githubusercontent.com/RubenHetKonijn/musicify/main/musicify.lua")
+shell.run("wget https://raw.githubusercontent.com/SquidDev-CC/mbs/master/mbs.lua .mbs.lua")
