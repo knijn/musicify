64a8cccfb17235705c942676d4188d8b0769a051
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-01-01 11:15:57 +0100

    Update musicify.lua

--- musicify.lua
+++ musicify.lua
@@ -1,9 +1,8 @@
 local indexURL = "https://raw.githubusercontent.com/RubenHetKonijn/computronics-songs/main/index.json?cb=" .. os.epoch("utc")
 local version = 0.041
 
-
 if peripheral.find("tape_drive") == false then
-  print("You should have a tape drive from computronics connected to the pc with a tape in it")
+  print("ERROR: You should have a tape drive from computronics connected to the pc with a tape in it")
 end
 local tape = peripheral.find("tape_drive")
 
@@ -12,7 +11,7 @@ local indexJSON = handle.readAll()
 handle.close()
 local index = textutils.unserialiseJSON(indexJSON)
 if not index then
-  print("The index seems to be malformed, make an issue on github or dm RubenKnijn#0043 on Discord")
+  print("ERROR: The index seems to be malformed, make an issue on github or dm RubenKnijn#0043 on Discord")
   return
 end
 local args = {...}
@@ -31,8 +30,26 @@ local function wipe()
   tape.seek(-90000)
 end
 
+
+local function play(songID)
+print("Writing and playing " .. songID.author .. " - " .. songID.name)
+  wipe()
+  tape.stop()
+  tape.seek(-tape.getSize()) -- go back to the start
+
+  local h = http.get(songID.file, nil, true) -- write in binary mode
+  tape.write(h.readAll()) -- that's it
+  h.close()
+
+  tape.seek(-tape.getSize()) -- back to start again
+
+  tape.setSpeed(songID.speed)
+  tape.play()
+end
+
+
 if version < index.latestVersion then
-  print("Your client is not up to date, please consider updating")
+  print("Your client is not up to date, please consider updating") -- Update check
 end
 
 if #args <= 0 then
@@ -44,7 +61,7 @@ if args[1] == "help" then
   print("musicify help  -- Displays this menu")
   print("musicify list -- Displays a list of songs you can play")
   print("musicify play <id> -- Plays the specified song")
-  print("musicify play shuffle -- Starts shuffle mode")
+  print("musicify shuffle [from] [to] -- Starts shuffle mode in a range if specified")
   print("musicify stop -- Stops playback")
 elseif args[1] == "stop" then
   print("Stopping playback")
@@ -54,47 +71,45 @@ elseif args[1] == "list" then
   for i in pairs(index.songs) do
     print(i .. " | " .. index.songs[i].author .. " - " .. index.songs[i].name)
   end
-elseif args[1] == "play" then
-  if args[2] == "shuffle" then
+elseif args[1] == "shuffle" then
+      if not args[2] then
+        from = 1
+      else
+        from = args[2]
+      end
+      if not args[3] then
+        to = #index.songs -- check if a range is supplied otherwise set it to the full range
+      else
+        to = args[3]
+      end
+
     while true do
-      local ranNum = math.random(1,#index.songs)
       print("Currently in shuffle mode, do <CTRL>+T to stop the shuffle")
-      print("Writing and playing " .. index.songs[ranNum].author .. " - " .. index.songs[ranNum].name)
-      wipe()
-      tape.stop()
-      tape.seek(-tape.getSize()) -- go back to the start
-
-      local h = http.get(index.songs[ranNum].file, nil, true) -- write in binary mode
-      tape.write(h.readAll()) -- that's it
-      h.close()
-
-      tape.seek(-tape.getSize()) -- back to start again
-
-      tape.setSpeed(index.songs[ranNum].speed)
-      tape.play()
+      if not args[3] then
+        from = 1
+      end
+      if not args[4] then
+        to = #index.songs
+      end
+      print(from)
+      print(to)
+      local ranNum = math.random(from,to)
+      play(index.songs[ranNum])
       sleep(index.songs[ranNum].time)
-    end
-  end
+      end
+elseif args[1] == "play" then
   if tonumber(args[2]) <= 0 then
-    print("Please specify a song id, use the `list` action to see all music in a list")
+    print("ERROR: Please specify a song id, use the `list` action to see all music in a list")
+    return
+  elseif tonumber(args[2]) > #index.songs then
+    print("ERROR: The number you specified is out of bounds, try a lower number")
     return
   end
   if not tape.isReady() then
-    print("You need to have a tape in the tape drive")
+    print("ERROR: You need to have a tape in the tape drive")
     return
   end
   playID = tonumber(args[2])
-  print("Writing and playing " .. index.songs[playID].author .. " - " .. index.songs[playID].name)
-  wipe()
-  tape.stop()
-  tape.seek(-tape.getSize()) -- go back to the start
-
-  local h = http.get(index.songs[playID].file, nil, true) -- write in binary mode
-  tape.write(h.readAll()) -- that's it
-  h.close()
-
-  tape.seek(-tape.getSize()) -- back to start again
-
-  tape.setSpeed(index.songs[playID].speed)
+  play(index.songs[playID])
   tape.play()
 end
