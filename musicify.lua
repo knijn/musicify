a7a2d9d6c5c7863143cc5da174301df2c7ca1a54
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-03-13 14:57:01 +0100

    Adding Artist search for listing and play all function

--- musicify.lua
+++ musicify.lua
@@ -128,8 +128,40 @@ musicify.stop = function (arguments)
     tape.stop()
 end
 
+local getArtistList = function()
+    local artistList = {}
+    for i,o in pairs(index.songs) do
+        for i2,o2 in pairs(artistList) do
+            if o2 == o then
+                debug("Found Duplicate")
+                return
+            end
+        end
+        table.insert(artistList,index.songs[i].author)
+    end
+    return artistList
+end
+
+local printArtistSongs = function(artist)
+    for i in pairs(index.songs) do
+        if index.songs[i].author == artist then
+            print(i .. " | " .. index.songs[i].author .. " - " .. index.songs[i].name)
+        end
+    end
+end
+
 musicify.list = function (arguments)
-    print("Format: `ID | Author - Name")
+    if not arguments then
+        arguments[1] = uhgaeoygu
+    end
+    print("Format: `ID | Author - Name`")
+    local artists = getArtistList()
+    for i,o in pairs(artists) do
+        if arguments[1] == artists[i] then
+            printArtistSongs(artists[i])
+            return
+        end
+    end
     for i in pairs(index.songs) do -- Loop through all songs
         print(i .. " | " .. index.songs[i].author .. " - " .. index.songs[i].name)
     end
@@ -177,6 +209,23 @@ musicify.volume = function (arguments)
 end
 
 musicify.play = function (arguments)
+    local artists = getArtistList()
+    local songList = {}
+    if arguments[1] == "all" then
+        for i2,o2 in pairs(index.songs) do
+            local songID = "," .. tostring(i2)
+            table.insert(songList,songID)
+        end
+        local handle = fs.open(".musicifytmp","w")
+        for i,o in pairs(songList) do
+            local song = "," .. songList[i]
+            handle.write(song)
+        end
+        handle.close()
+        musicify.playlist({".musicifytmp"})
+        return
+    end
+
     if not arguments then
         print("Resuming playback...")
         return
