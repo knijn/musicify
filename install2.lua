69bddb962583d7072f5782cd0fedbed6cc6e766b
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-06-23 15:00:12 +0200

    add basic code (untested)

--- install2.lua
+++ install2.lua
@@ -1,4 +1,9 @@
 local apiEndpoint = "https://api.github.com/repos/knijn/musicify/releases"
 -- data[1].assets[1].browser_download_url
+local handle = http.get(apiEndpoint)
+local data = textutils.unserialiseJSON(handle.readAll))
+handle.close()
+
+local url = data[1].assets[1].browser_download_url
 
 -- WIP, to be finished!!
