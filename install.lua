fc462ba63428fcad764d0fd97ec1ab6a3742532c
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-06-23 19:12:32 +0200

    change names for install scripts

--- install.lua
+++ install.lua
@@ -1,4 +1,39 @@
-shell.run("wget https://raw.githubusercontent.com/knijn/musicify/main/musicify.lua")
-shell.run("wget https://raw.githubusercontent.com/knijn/musicify/main/musicify_config.json")
-shell.run("wget https://raw.githubusercontent.com/SquidDev-CC/mbs/master/mbs.lua .mbs.lua")
-shell.run(".mbs.lua install")
+-- data[1].assets[1].browser_download_url
+local apiURL = "http://api.github.com/repos/knijn/musicify/releases"
+local args = {...}
+local skipcheck = false
+if args and args[1] == "y" then
+  skipcheck = true
+end
+
+local scKey = _G._GIT_API_KEY
+if scKey then
+  requestData = {
+    url = apiURL,
+    headers = {["Authorization"] = "token " .. scKey}
+  }
+  http.request(requestData)
+else
+  http.request(apiURL) -- when not on switchcraft, use no authentication
+end
+print("Made request")
+
+while true do
+  event, url, handle = os.pullEvent()
+  if event == "http_failure" then
+    error("Failed to download file: " .. handle)
+  elseif event == "http_success" then
+    print(handle.getResponseCode())
+    local data = textutils.unserialiseJSON(handle.readAll())
+    local url = data[1].assets[1].browser_download_url
+    print("Downloading Musicify from: " .. url .. ", is this okay? (n to cancel, anything else to continue)")
+    local input = read()
+    if not skipcheck and input == keys.n then
+      error("Cancelled Installation")
+    end
+    print("Installing now")
+    shell.run("wget " .. url)
+
+    return
+  end
+end
