97c808588d0c22ec5112b4024fc6bb3850247c6f
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2022-06-23 18:22:49 +0200

    make the final version for 1.0 release

--- install2.lua
+++ install2.lua
@@ -1,9 +1,39 @@
-local apiEndpoint = "https://api.github.com/repos/knijn/musicify/releases"
 -- data[1].assets[1].browser_download_url
-local handle = http.get(apiEndpoint)
-local data = textutils.unserialiseJSON(handle.readAll))
-handle.close()
+local apiURL = "http://api.github.com/repos/knijn/musicify/releases"
+local args = {...}
+local skipcheck = false
+if args and args[1] == "y" then
+  skipcheck = true
+end
 
-local url = data[1].assets[1].browser_download_url
+local scKey = _G._GIT_API_KEY
+if scKey then
+  requestData = {
+    url = apiURL,
+    headers = {["Authorization"] = "token " .. scKey}
+  }
+  http.request(requestData)
+else
+  http.request(apiURL) -- when not on switchcraft, use no authentication
+end
+print("Made request")
 
--- WIP, to be finished!!
+while true do
+  event, url, handle = os.pullEvent()
+  if event == "http_failure" then
+    error("Failed to download file: " .. handle)
+  elseif event == "http_success" then
+    print(handle.getResponseCode())
+    local data = textutils.unserialiseJSON(handle.readAll())
+    local url = data[1].assets[1].browser_download_url
+    print("Downloading Musicify from: " .. url .. ", is this okay? (n to cancel, anything else to continue)")
+    local input = read()
+    if not skipcheck and input == keys.n then
+      error("Cancelled Installation")
+    end
+    print("Installing now")
+    shell.run("wget " .. url)
+
+    return
+  end
+end
