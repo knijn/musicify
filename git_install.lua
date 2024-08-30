11d87914dfd91607b75c6b8a802dd80d3583a748
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-08-30 08:25:20 +0200

    fix git_install

--- git_install.lua
+++ git_install.lua
@@ -1,4 +1,2 @@
+shell.run("rm musicify.lua")
 shell.run("wget https://raw.githubusercontent.com/knijn/musicify/main/musicify.lua")
-shell.run("wget https://raw.githubusercontent.com/knijn/musicify/main/musicify_config.json")
-shell.run("wget https://raw.githubusercontent.com/SquidDev-CC/mbs/master/mbs.lua .mbs.lua")
-shell.run(".mbs.lua install")
